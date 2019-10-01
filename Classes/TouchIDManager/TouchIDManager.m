9581c5a00a43ab5a7c91ac99220ee71498706305
Barbara Vujicic <barbara.vujicic@infinum.hr>
2019-10-01 15:23:18 +0200

    FaceID devices updated

--- Classes/TouchIDManager/TouchIDManager.m
+++ Classes/TouchIDManager/TouchIDManager.m
@@ -136,7 +136,7 @@
     struct utsname systemInfo;
     uname(&systemInfo);
     NSString *code = [NSString stringWithCString:systemInfo.machine encoding:NSUTF8StringEncoding];
-    NSArray *faceIdDevices = @[@"iPhone10,3", @"iPhone10,6", @"iPhone11,2", @"iPhone11,4", @"iPhone11,6", @"iPhone11,8"];
+    NSArray *faceIdDevices = @[@"iPhone10,3", @"iPhone10,6", @"iPhone11,2", @"iPhone11,4", @"iPhone11,6", @"iPhone11,8", @"iPhone12,1", @"iPhone12,3", @"iPhone12,5"];
     
     return [faceIdDevices containsObject:code];
 }
