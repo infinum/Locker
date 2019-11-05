1a803664e728ecf8cc81d33cbefe54e45e54c78f
Jasmin Abou Aldan <jasmin.aboualdan@infinum.hr>
2019-11-05 12:05:32 +0100

    Add Locker tests

--- Example/TouchIDTests/TouchIDTests.swift
+++ Example/TouchIDTests/TouchIDTests.swift
@@ -8,29 +8,121 @@
 
 import XCTest
 @testable import TouchID
+import Locker
 
 class TouchIDTests: XCTestCase {
-    
+
+    private var containerViewController: ViewController!
+
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        containerViewController = ViewController()
     }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+
+    // MARK: - Store Retrieve and Delete -
+    func testStoreAndRetrieveSecret() {
+
+        // Store
+        containerViewController.storeSecret()
+
+        // Retrieve
+        let asyncExpectation = expectation(description: "Async block executed")
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+            self.containerViewController.readSecret(success: { (secret) in
+                XCTAssertEqual(secret, self.containerViewController.topSecret)
+                asyncExpectation.fulfill()
+            }, failure: { _ in
+                XCTFail("Error while retrieve the secret")
+                asyncExpectation.fulfill()
+            })
         }
+
+        waitForExpectations(timeout: 3, handler: nil)
     }
+
+    func testDeleteSecret() {
+
+        // Store
+        containerViewController.storeSecret()
+
+        // Delete
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+            self.containerViewController.deleteSecret()
+        }
+
+        // Retrieve
+        let asyncExpectation = expectation(description: "Async block executed")
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+            self.containerViewController.readSecret(success: { (secret) in
+                XCTFail("Data should not be available")
+                asyncExpectation.fulfill()
+            }, failure: { _ in
+                XCTAssertTrue(true, "Data succesfully deleted")
+                asyncExpectation.fulfill()
+            })
+        }
+
+        waitForExpectations(timeout: 3, handler: nil)
+    }
+
+    // MARK: - Device settings -
+
+    func testSettingsChanged() {
+        XCTAssertFalse(containerViewController.settingsChanged)
+    }
+
+    // MARK: - Helpers -
     
+    func testShouldUseAuthFlag() {
+        containerViewController.shouldUseAuthWithBiometrics = true
+        XCTAssertTrue(containerViewController.shouldUseAuthWithBiometrics)
+    }
+
+    func testDidAskToUseAuthWithBiometrics() {
+        containerViewController.didAskToUseAuthWithBiometrics = true
+        XCTAssertTrue(containerViewController.didAskToUseAuthWithBiometrics)
+    }
+
+    func testShouldAddSecretToKeychainOnNextLogin() {
+        containerViewController.shouldAddSecretToKeychainOnNextLogin = true
+        XCTAssertTrue(containerViewController.shouldAddSecretToKeychainOnNextLogin)
+    }
+
+    // MARK: - Reseting -
+
+    func testResetAll() {
+
+        // Store some data
+        containerViewController.shouldUseAuthWithBiometrics = true
+        containerViewController.didAskToUseAuthWithBiometrics = true
+        containerViewController.shouldAddSecretToKeychainOnNextLogin = true
+        containerViewController.storeSecret()
+
+        // Reset locker
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+            self.containerViewController.resetEverything()
+        }
+
+        // Check if everything is reseted
+        let asyncExpectation = expectation(description: "Async block executed")
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+            self.containerViewController.readSecret(success: { (secret) in
+                XCTFail("Data should not be available")
+                asyncExpectation.fulfill()
+            }, failure: { _ in
+
+                let shouldUseAuth = self.containerViewController.shouldUseAuthWithBiometrics
+                let didAskToUseAuth = self.containerViewController.didAskToUseAuthWithBiometrics
+                let shouldAddSecretOnNextLogin = self.containerViewController.shouldAddSecretToKeychainOnNextLogin
+
+                let allReseted = shouldUseAuth || didAskToUseAuth || shouldAddSecretOnNextLogin
+
+                XCTAssertFalse(allReseted, "Locker succesfully reseted")
+
+                asyncExpectation.fulfill()
+            })
+        }
+        waitForExpectations(timeout: 3, handler: nil)
+    }
 }
