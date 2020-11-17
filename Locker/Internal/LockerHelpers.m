1fc4e152aaa2fcc5cf1111c3697b69dc0ac5f497
gitsino <sinisa.abramovic@infinum.hr>
2020-11-17 15:25:07 +0100

    Added update to list of devices that support FaceID

--- Locker/Internal/LockerHelpers.m
+++ Locker/Internal/LockerHelpers.m
@@ -175,7 +175,7 @@
         return YES;
     }
 
-    NSArray *faceIdDevices = @[@"iPhone10,3", @"iPhone10,6", @"iPhone11,2", @"iPhone11,4", @"iPhone11,6", @"iPhone11,8", @"iPhone12,1", @"iPhone12,3", @"iPhone12,5"];
+    NSArray *faceIdDevices = @[@"iPhone10,3", @"iPhone10,6", @"iPhone11,2", @"iPhone11,4", @"iPhone11,6", @"iPhone11,8", @"iPhone12,1", @"iPhone12,3", @"iPhone12,5", @"iPhone13,1", @"iPhone13,3", @"iPhone13,4", @"iPad8,9", @"iPad8,1", @"iPad8,2", @"iPad8,3", @"iPad8,4", @"iPad8,5", @"iPad8,6", @"iPad8,7", @"iPad8,8", @"iPad8,9", @"iPad8,10", @"iPad8,11", @"iPad8,12"];
 
     return [faceIdDevices containsObject:LockerHelpers.deviceCode];
 }
