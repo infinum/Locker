121f66bf7ba337fb1ce4ea652518e57eec0ecf36
Zvonimir Medak <zvonimir.medak@infinum.com>
2021-10-21 13:52:48 +0200

    initialized Package.swift and modified .gitignore file

---
+++ Sources/Locker/Locker.swift
@@ -0,0 +1,3 @@
+struct Locker {
+    var text = "Hello, World!"
+}
---
+++ Tests/LockerTests/LockerTests.swift
@@ -0,0 +1,11 @@
+    import XCTest
+    @testable import Locker
+
+    final class LockerTests: XCTestCase {
+        func testExample() {
+            // This is an example of a functional test case.
+            // Use XCTAssert and related functions to verify your tests produce the correct
+            // results.
+            XCTAssertEqual(Locker().text, "Hello, World!")
+        }
+    }
